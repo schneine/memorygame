554cb0ebad5f511eb9bb16edb81e8b320ff68c06
schneine <nelly.helen.schneider@hs-furtwangen.de>
2021-06-09 18:49:48 +0200

    build

---
+++ Drumpad_local/Server/server.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const WebSocket = require("ws");
+// // create WebSocket server with given port
+const port = Number(process.env.PORT) || 8000;
+const server = new WebSocket.Server({ port: port });
+// set of connected sockets
+const clientSockets = new Set();
+// init counters (0 is number of connected clients)
+const counters = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+server.on("connection", (socket) => {
+    console.log("connected");
+    console.log("hello world");
+});
+//# sourceMappingURL=server.js.map
